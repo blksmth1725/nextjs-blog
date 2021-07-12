302c2153821286fee30b28f664c76ca7085baba0
Christian Sheen <csheen1725@gmail.com>
2021-07-12 18:35:37 -0400

    Using the Image tag and exploring how metaData works in nextjs

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,16 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const FirstPost = () => {
   return (
     <>
+      <Image
+        src="/images/killer-mike-john-henry.png"
+        height={144}
+        width={210}
+        alt="The Blk Smth"
+      />
+
       <h1>First Post</h1>
       <h2>
         <Link href="/">
